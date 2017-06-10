fb750a47091103b58dbf490237ea57f219e3124b
Stelios Voskos <s.voskos@googlemail.com>
2017-06-10 23:38:29 +0100

    Added the correct path to action creators

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,6 +1,6 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import * as actions from './actionCreators/actions';
+import * as actions from '../actionCreators/actions';
 import App from '../components/App';
 
 function mapStateToProps(state) {
